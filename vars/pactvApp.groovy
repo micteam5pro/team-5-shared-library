bf7108d04b233c1e57f5a3f576ea70493d9ad70e
Mmdpactv <127458439+Mmdpactv@users.noreply.github.com>
2023-04-13 14:11:35 -0500

    Create pactvApp.groovy

---
+++ vars/pactvApp.groovy
@@ -0,0 +1,34 @@
+def call(string repoUrl){
+  pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage('team5healthapp-shared'){
+            steps{
+                echo "Welcome Engr michael app"
+            }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
+}
+}
+
+  
